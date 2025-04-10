9b6f47fb947e07a5d2c6fb6673b3b56136c3629f
suman-code-art <sumantripathi396@gmail.com>
2025-04-11 01:24:12 +0530

    Initial commit

---
+++ tempCodeRunnerFile.bat
@@ -0,0 +1,11 @@
+git init
+
+git add index.html
+git add style.css
+git add app.js
+git commit -m "Initial commit"
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
+
